0885f69e2e50fd2e980e22689ba11bc464488574
ssisaias <ssisaias@users.noreply.github.com>
2024-04-14 18:59:44 -0300

    Rather than redirecting, display an error message when there's no session id param

--- src/app/checkout-success/checkout-success.tsx
+++ src/app/checkout-success/checkout-success.tsx
@@ -8,33 +8,57 @@ export const metadata: Metadata = {
 };
 
 interface SuccessClientPageProps {
-  checkoutDetails: Stripe.Response<Stripe.Checkout.Session>
+  checkoutDetails: Stripe.Response<Stripe.Checkout.Session>;
 }
 
-export default async function SuccessClientPage({ checkoutDetails }: SuccessClientPageProps) {
-  const customerName = checkoutDetails.customer_details?.name;
-  const product = checkoutDetails.line_items?.data[0].price?.product as Stripe.Product
+export default async function SuccessClientPage({
+  checkoutDetails,
+}: SuccessClientPageProps) {
+  let customerName;
+  let product;
+  const noData = !checkoutDetails;
+  if (checkoutDetails) {
+    customerName = checkoutDetails.customer_details?.name;
+    product = checkoutDetails.line_items?.data[0].price
+      ?.product as Stripe.Product;
+  }
 
   return (
     <main className="m-auto flex flex-col items-center justify-center">
-      <h1 className="text-xxl text-gray-100 font-bold mb-14">Compra efetuada!</h1>
+      {noData ? (
+        <>
+          <h1 className="mb-14 text-xxl font-bold text-gray-100">
+            Compra não efetuada!
+          </h1>
+          <h1 className="mb-14 flex items-center justify-center text-lg text-gray-100">
+            Encontramos um erro, e já estamos trabalhando para resolver! Tente
+            de novo mais tarde.
+          </h1>
+        </>
+      ) : (
+        <>
+          <h1 className="mb-14 text-xxl font-bold text-gray-100">
+            Compra efetuada!
+          </h1>
 
-      <ImageContainer
-        imageUrl={product.images[0]}
-        alt=""
-        width={125}
-        height={125}
-        className="flex items-center justify-center rounded-lg p-1 max-w-48"
-      />
+          <ImageContainer
+            imageUrl={product?.images[0]!}
+            alt=""
+            width={125}
+            height={125}
+            className="flex max-w-48 items-center justify-center rounded-lg p-1"
+          />
 
-      <p className="text-lg text-gray-300 mt-10 mb-6 text-center leading-5">
-      ¡¡Uhul, <strong>{customerName}</strong>, sua{" "}
-        <strong>{product.name}</strong> já ta chegando!!
-      </p>
+          <p className="mb-6 mt-10 text-center text-lg leading-5 text-gray-300">
+            ¡¡Uhul, <strong>{customerName}</strong>, sua{" "}
+            <strong>{product?.name}</strong> já ta chegando!!
+          </p>
 
-      <Link href="/" className="text-violet-500 hover:text-violet-300">
-        Voltar para a página inicial
-      </Link>
+          <Link href="/" className="text-violet-500 hover:text-violet-300">
+            Voltar para a página inicial
+          </Link>
+        </>
+      )}
     </main>
   );
 }
--- src/app/checkout-success/page.tsx
+++ src/app/checkout-success/page.tsx
@@ -1,22 +1,9 @@
 import { stripe } from "@/lib/stripe";
-import Stripe from "stripe";
 
 import SuccessClientPage from "./checkout-success";
-import { useRouter } from "next/navigation";
-import { log } from "console";
-/* localhost:3000/checkout-success?session_id=cs_test_a1NRUJ8NynPulaThUlv4vCTuh54db7UUK7gLADT5lgizNutcCA1HJkjqsI */
-
-type ProductData = {
-  id: string;
-  name: string;
-  imageUrl: string;
-  description: string;
-  price: string;
-  defaultPriceId: string;
-};
 
 async function getCheckoutDetails(sessionId: string) {
-  if (!sessionId) return;
+  if (!sessionId) return null;
 
   const response = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ["customer", "line_items.data.price.product"],
