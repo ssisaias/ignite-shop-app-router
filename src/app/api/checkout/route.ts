189357ffe6ffa6b4898ef4c7dfc37e7c1371d3da
ssisaias <ssisaias@users.noreply.github.com>
2024-04-14 17:22:59 -0300

    Adds API route that requests a new checkout session from Stripe

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,34 @@
+import { stripe } from "@/lib/stripe";
+import { log } from "console";
+import { STATUS_CODES } from "http";
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(request: NextApiRequest) {
+  log(request);
+  const priceId = new URL(request.url||'').searchParams.get('priceId');
+  if (!priceId) {
+    return NextResponse.json(new Error('PriceId is required'), {status: 400});
+  }
+
+  const sucessUrl = process.env.NEXT_URL + "/sucess";
+  const cancelUrl = process.env.NEXT_URL + "/";
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: "payment",
+    success_url: sucessUrl,
+    cancel_url: cancelUrl,
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  });
+
+  //console.log(checkoutSession);
+  
+  return NextResponse.json({
+    checkoutUrl: checkoutSession.url,
+  }, {status: 201, headers: {'Content-Type': 'application/json'}});
+}
